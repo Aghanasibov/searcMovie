cab2d5582a37113bd821460d9763af4549b900e8
Aghanasibov <142959441+Aghanasibov@users.noreply.github.com>
2023-12-30 18:21:16 +0400

    uptade

--- main.js
+++ main.js
@@ -1,46 +1,4 @@
-//
-// const searchBtn=document.querySelector('#searchBtn')
-// const searchInput=document.querySelector('#searchInput')
-//
-//
-// const movieImg=document.querySelector('#movieImg')
-// const movieCard=document.querySelector('#movieCard')
-// const movieSubtitle=document.querySelector('#movieSubtitle')
-// const movieDesc=document.querySelector('#movieDesc')
-//
-//
-//
-//
-//
-//
-//
-//
-// searchBtn.addEventListener('click',function () {
-//     const movieTitle=searchInput.value
-//     console.log('movieTitle',movieTitle)
-//     const myPromise = fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=2c4d035c&t=${movieTitle}`)
-//
-//
-//
-//     myPromise.then(res => {
-//         const secondPromise = res.json()
-//         return secondPromise
-//     }).then((movieObj) => {
-//         console.log(movieObj);
-//
-//         movieImg.src=movieObj.Poster
-//         movieCard.innerHTML=`${movieObj.Title} ${movieObj.Relased}`
-//         movieSubtitle.innerHTML=movieObj.Actors
-//         movieDesc.innerHTML=movieObj.Awards
-//     }).catch((err) => {
-//         console.log('err', err)
-//     }).finally(() => {
-//         console.log('fetched')
-//     })
-//
-//
-//     searchInput.value=''
-// })
+
 
 const searchBtn = document.querySelector('#searchBtn');
 const searchInput = document.querySelector('#searchInput');
@@ -67,7 +25,7 @@ searchBtn.addEventListener('click', async function () {
         }
     } catch (err) {
         console.error('Sorğuda səhv:', err.message);
-        movieImg.src = ''; // Hata durumunda resmi temizle
+        movieImg.src = '';
         movieCard.textContent = 'Film tapılmadı';
         movieSubtitle.textContent = '';
         movieDesc.textContent = '';
